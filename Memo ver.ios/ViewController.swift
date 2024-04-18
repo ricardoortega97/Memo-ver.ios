c9682838d675804e4f8ff036543cac3209b5ea01
Ricardo Ortega Pacheco <ricardo.ortega97@outlook.com>
2024-04-18 19:33:56 -0400

    uh

--- Memo ver.ios/ViewController.swift
+++ Memo ver.ios/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var outlet: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
