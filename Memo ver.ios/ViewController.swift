fd59eb6ae760bc803aaf4c5f7169945d9f23c0db
Ricardo Ortega Pacheco <ricardo.ortega97@outlook.com>
2024-04-18 19:24:51 -0400

    Initial Commit

---
+++ Memo ver.ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memo ver.ios
+//
+//  Created by Ricardo Ortega on 4/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
