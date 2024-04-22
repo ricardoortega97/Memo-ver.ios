9352e7acfc1f685d813246a6c4891cc9d5216632
Ricardo Ortega Pacheco <ricardo.ortega97@outlook.com>
2024-04-22 14:32:00 -0400

    "Init Blueprint"

---
+++ Memo ver.ios/MemoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MemoViewController.swift
+//  Memo ver.ios
+//
+//  Created by Ricardo Ortega on 4/22/24.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
