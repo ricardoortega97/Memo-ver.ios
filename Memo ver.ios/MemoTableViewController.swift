91e01ac71412e6e42da4d1e757dd2c9d8d46d705
asaji1 <asaji1@student.gsu.edu>
2024-04-23 03:46:38 -0400

    Updated MemoTableViewController

--- Memo ver.ios/MemoTableViewController.swift
+++ Memo ver.ios/MemoTableViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 import CoreData
 
 class MemoTableViewController: UITableViewController {
-
+    
+    var memos: [NSManagedObject]  = []
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadDataFromDatabase()
+        
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -19,17 +25,89 @@ class MemoTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadDataFromDatabase()
+        tableView.reloadData()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func loadDataFromDatabase() {
+        //read settings to enable sorting
+        let settings = UserDefaults.standard
+        let sortField = settings.string(forKey: Constants.kSortField)
+        let sortAscending = settings.bool(forKey: Constants.kSortDirectionAscending)
+        
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSManagedObject>{entityName: "Memo"}
+        //specify sorting
+        let sortDescriptor = NSSortDescriptor(key: sortField, ascending: sortAscending)
+        let sortDescriptorArray = [sortDescriptor]
+        request.sortDescriptors = sortDescriptorArray
+        do {
+            memos = try context.fetch(request)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return memos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemosCell", for: indexPath)
+        
+        let memo = memos[indexPath.row] as? Memo
+        
+        cell.textLabel?.text = memo?.memoSubject
+        cell.detailTextLabel.text = memo?.memoDate
+        cell.accessoryType = .detailDisclosureButton
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EditMemo" {
+            let memoController = segue.destination as? MemoViewController
+            let selectedRow = self.tableView.indexPath(for: sender as! UITableViewCell)?.row
+            let selectedMemo = memos[selectedRow!] as? Memo
+            memoController?.currentMemo = selectedMemo!
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedMemo = memos[indexPath.row] as? Memo
+        let subject = selectedMemo.memoSubject
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "MemoController") as? MemoViewController
+        controller?.currentMemo = selectedMemo
+        self.navigationController?.pushViewController(controller!, animated: true)
+        
+        let alertController = UIAlertController(title: "Memo selected", message: "Selected row: \(indexPath.row) (\(subject))", preferredStyle: .alert)
+        
+        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let actionDetails = UIAlertAction(title: "Show Details", style: .default, handler: actionHandler)
+        
+        alertController.addAction(actionCancel)
+        alertController.addAction(actionDetails)
+        present(alertController, animated: true, completion: nil)
+        
+        
+        
     }
 
     /*
@@ -50,17 +128,26 @@ class MemoTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let memo = memos[indexPath.row] as? Memo
+            let context = appDelegate.persistentContainer.viewContext
+            context.delete(memo!)
+            do {
+                try context.save()
+            }
+            catch {
+                fatalError("Error saving context: \(error)")
+            }
+            loadDataFromDatabase()
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
